abae0fa88422f1fac93e6ea50d498b49d89e8654
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-12-31 08:48:05 -0600

    update buttons

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,46 @@
-document.getElementById('closeButton').addEventListener('click', () => {
-  window.electron.closeWindows();
+import { playSound, changeLight } from './av.js';
+
+function changePage(page) {
+  window.electron.changePage(page);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Clear localStorage
+  localStorage.clear();
+
+  document.getElementById('closeButton').addEventListener('click', () => {
+    window.electron.closeWindows();
+  });
+
+  document.getElementById('goToLogoButton').addEventListener('click', () => {
+    changePage('');
+  });
+
+  document.getElementById('goToTimersButton').addEventListener('click', () => {
+    changePage('timers');
+  });
+
+  document
+    .getElementById('goToLeaderboardsButton')
+    .addEventListener('click', () => {
+      changePage('leaderboards');
+    });
+
+  document
+    .getElementById('changeLightsWhiteButton')
+    .addEventListener('click', () => {
+      ['1', '2', '3'].forEach((index) => changeLight(index, 'reset'));
+    });
+
+  document
+    .getElementById('changeLightsRedButton')
+    .addEventListener('click', () => {
+      ['1', '2', '3'].forEach((index) => changeLight(index, 'red'));
+    });
+
+  document
+    .getElementById('turnOffLightsButton')
+    .addEventListener('click', () => {
+      ['1', '2', '3'].forEach((index) => changeLight(index, 'off'));
+    });
 });
