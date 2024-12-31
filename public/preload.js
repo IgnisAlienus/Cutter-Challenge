e7f40898ae23288bc849b8ffd6c9af03842515dd
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-12-31 08:48:28 -0600

    update preload for electron

--- public/preload.js
+++ public/preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   closeWindows: () => ipcRenderer.send('close-windows'),
+  changePage: (page) => ipcRenderer.send('change-page', page),
 });
